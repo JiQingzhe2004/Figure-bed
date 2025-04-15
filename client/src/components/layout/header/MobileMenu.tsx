735eb430602fe5c9d7be8fa48df8204444763234
JiQingzhe2004 <jin648862@gmail.com>
2025-04-15 20:50:06 +0800

    去掉未使用的导入

--- client/src/components/layout/header/MobileMenu.tsx
+++ client/src/components/layout/header/MobileMenu.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import UserAvatar from './UserAvatar';
 import ThemeSwitcher from '../../ui/ThemeSwitcher';
-
 interface MobileMenuProps {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
